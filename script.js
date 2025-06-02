e9929cdbeea7aa4f778d4d0c8d91ef625945e3f8
AlirezaSaei1 <saeialireza359@gmail.com>
2025-06-02 16:15:20 +0300

    Added profile pic and dark/light mode toggle

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Website loaded successfully!");
+
+const toggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+toggle.addEventListener('change', () => {
+  body.classList.toggle('dark-mode');
+});
