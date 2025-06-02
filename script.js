fdf0161dfbd35aaf42b4b65da378f86213679323
AlirezaSaei1 <saeialireza359@gmail.com>
2025-06-02 17:05:32 +0300

    Added scroll bar

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const body = document.body;
 toggle.addEventListener('change', () => {
   body.classList.toggle('dark-mode');
 });
+
+const progressBar = document.getElementById("scrollProgress");
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+  progressBar.style.width = `${scrollPercent}%`;
+});
